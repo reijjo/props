0e6cdc60d35a7a3aeaed94c1bb0882d6fb190e6f
Teemu Aitomeri <95418273+reijjo@users.noreply.github.com>
2025-11-13 19:29:13 +0200

    Delete client/.svelte-kit directory

--- client/.svelte-kit/output/client/_app/immutable/chunks/DdP17-ek.js
+++
@@ -1,1 +0,0 @@
-import{q as a}from"./BZYip4vB.js";a();
--- client/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.C8-dSHpi.js","_app/immutable/chunks/DIn6DAE4.js","_app/immutable/chunks/BZYip4vB.js","_app/immutable/chunks/Da-o-VMT.js","_app/immutable/chunks/DdP17-ek.js","_app/immutable/chunks/CIhVivWY.js","_app/immutable/chunks/Biei5fb3.js"];
-export const stylesheets = ["_app/immutable/assets/0.BCberTiI.css"];
-export const fonts = [];
