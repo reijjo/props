520f85931145df870924dc88ef2dcf57c5759a19
Reijjo <teemu.aitomeri@gmail.com>
2025-12-12 16:56:56 +0200

    fixed nav styles

--- sveltekit-client/src/lib/components/layout/Navbar/NavLinks.svelte
+++ sveltekit-client/src/lib/components/layout/Navbar/NavLinks.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import '$lib/styles/navbar.css';
 	import { page } from '$app/state';
 
 	const navLinks = [
@@ -25,12 +26,11 @@
 
 <ul class="nav-links">
 	{#each navLinks as link}
-		<li>
+		<li class="nav-item" class:has-submenu={!!link.sublinks}>
 			<a
 				href={link.href}
-				class:active={page.url.pathname.toString() === link.href}
-				aria-current={page.url.pathname.toString() === link.href ? 'page' : undefined}
-				>{link.label}</a
+				class:active={page.url.pathname === link.href}
+				aria-current={page.url.pathname === link.href ? 'page' : undefined}>{link.label}</a
 			>
 			{#if link.sublinks}
 				<ul class="nav-sub-menu">
@@ -44,79 +44,3 @@
 		</li>
 	{/each}
 </ul>
-
-<style>
-	.nav-links {
-		display: flex;
-		gap: 1.5rem;
-		position: relative;
-		height: 100%;
-		align-items: center;
-
-		& li {
-			list-style: none;
-
-			& a {
-				height: 100%;
-			}
-
-			& a.active {
-				font-weight: 600;
-			}
-
-			& a:hover:not(.active) {
-				color: var(--accent-200);
-			}
-		}
-	}
-
-	.nav-sub-menu {
-		visibility: hidden;
-		opacity: 0;
-		max-height: 0;
-		border: 1px solid rgba(255, 255, 255, 0.125);
-		border-radius: 0.5rem;
-		position: absolute;
-		top: calc(var(--nav-height));
-		left: 0;
-		width: max-content;
-		flex-direction: column;
-		background: linear-gradient(to bottom right, rgba(0, 0, 0, 0.125), rgba(0, 0, 0, 0.125));
-		box-shadow:
-			0 8px 16px rgba(0, 163, 218, 0.125),
-			0 8px 36px rgba(0, 163, 218, 0.075);
-
-		transform: scaleY(0);
-		transform-origin: top;
-
-		transition:
-			opacity 0.125s ease,
-			visibility 0.125s ease,
-			max-height 0.25s ease,
-			transform 0.25s ease;
-		overflow: hidden;
-
-		& li {
-			padding-block: 0.5rem;
-
-			&:hover {
-				background: var(--primary);
-			}
-
-			&:not(:last-child) {
-				border-bottom: 1px solid rgba(255, 255, 255, 0.125);
-			}
-
-			& a {
-				padding-inline: 1rem;
-			}
-		}
-	}
-
-	.nav-links:hover .nav-sub-menu {
-		opacity: 1;
-		max-height: 600px;
-		visibility: visible;
-		transform: scaleY(1);
-	}
-</style>
--- sveltekit-client/src/lib/components/layout/Navbar/Navbar.svelte
+++ sveltekit-client/src/lib/components/layout/Navbar/Navbar.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/Button.svelte';
-
-	import { page } from '$app/state';
 	import NavLinks from './NavLinks.svelte';
 </script>
 
@@ -17,35 +15,11 @@
 </nav>
 
 <style>
-	nav {
-		position: fixed;
-		z-index: 10;
-		top: 1rem;
-		left: 50%;
-		transform: translateX(-50%);
-		width: 95%;
-		max-width: 1366px;
-		height: var(--nav-height);
-
-		background: linear-gradient(to bottom right, rgba(0, 0, 0, 0.125), rgba(0, 0, 0, 0.125));
-		backdrop-filter: blur(1rem);
-		border: 1px solid rgba(255, 255, 255, 0.125);
-		border-radius: 5rem;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		padding-inline: 1.5rem;
-
-		box-shadow:
-			0 8px 16px rgba(0, 163, 218, 0.125),
-			0 8px 36px rgba(0, 163, 218, 0.075);
-	}
-
 	.nav-buttons {
 		display: flex;
 		gap: 1rem;
 		height: 2.5rem;
-		/* visibility: hidden; */
+
 		& :global(button) {
 			padding: 0.25rem 1.5rem;
 		}
