6b4a7397f11eb54ef280bc1eb680bba6d0ba672d
Reijjo <teemu.aitomeri@gmail.com>
2026-01-30 19:34:14 +0200

    Added server ping to start render.com

--- server/src/routes/mod.rs
+++ server/src/routes/mod.rs
@@ -6,6 +6,6 @@ use crate::app::AppState;
 
 pub fn init_routes() -> Router<AppState> {
     Router::new()
-        .route("/", get(|| async { "Hi im a placeholder" }))
+        .route("/", get(|| async { "Server running." }))
         .nest("/api/nba", nba::nba_routes())
 }
